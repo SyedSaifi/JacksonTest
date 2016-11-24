0a51d54c7efda89afa98fe2908422cc9d688bba7
syed saifi <syed.saifi@oracle.com>
2016-11-24 08:19:06 +0530

    initial commit

---
+++ src/com/eaxmple/json/Jackson2Read.java
@@ -0,0 +1,47 @@
+ 	package com.eaxmple.json;
+
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Jackson2Read {
+
+	public static void main(String[] args) {
+		Jackson2Read obj = new Jackson2Read();
+		obj.run();
+	}
+
+	private void run() {
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+
+			// Convert JSON string from file to Object
+			Staff staff = mapper.readValue(new File("staff.json"), Staff.class);
+			System.out.println(staff.getName());
+
+			// Convert JSON string to Object
+			String jsonInString = "{\"name\":\"mkyong\",\"salary\":7500,\"skills\":[\"java\",\"python\"]}";
+			Staff staff1 = mapper.readValue(jsonInString, Staff.class);
+			System.out.println(staff1);
+
+			//Pretty print
+			String prettyStaff1 = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(staff1);
+			System.out.println(prettyStaff1);
+
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
